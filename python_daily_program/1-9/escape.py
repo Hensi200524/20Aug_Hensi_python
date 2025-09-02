5133e095a46b497487ea943f0cf9e13e42ddc476
Hensi200524 <vaghelahensi@gmail.com>
2025-09-02 12:15:06 +0530

    prosave

---
+++ python_daily_program/1-9/escape.py
@@ -0,0 +1,9 @@
+#escape sequences
+print("Hello\nPython")
+print("Hello\tPython")
+print("Hello\\Python")
+print("Hello\bPython")
+print("hello 'python' ")
+print('hello "python" ')
+print("hello \"python\" ")
+print("hello \rpython")
